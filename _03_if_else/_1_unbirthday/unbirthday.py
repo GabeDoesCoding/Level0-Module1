7bd20ce9d31c67cdde239bb0e5852ad154a0aa09
GabeDoesCoding <gabelorensd@gmail.com>
2021-11-16 18:26:13 -0800

    working on remarkable

--- _03_if_else/_1_unbirthday/unbirthday.py
+++ _03_if_else/_1_unbirthday/unbirthday.py
@@ -3,4 +3,10 @@ from tkinter import messagebox, simpledialog, Tk
 
 
 if __name__ == '__main__':
-    bday = messagebox.askstring(title='Birthday', prompt ="What is your birthday? (Answer in MM/DD format)")
+    window = Tk()
+    window.withdraw()
+    bday = simpledialog.askstring(title='Birthday', prompt ="What is your birthday? (Answer in MM/DD format)")
+    if bday == '11/16':
+        messagebox.showinfo(title='Unbirthday', message='Have a happy birthday!')
+    else:
+        messagebox.showinfo(title='Unbirthday', message='Have a very merry UNbirthday')
