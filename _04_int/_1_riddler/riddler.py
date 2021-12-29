725c7f10186e4642504b6dc1c772ea9894e28e0d
GabeDoesCoding <gabelorensd@gmail.com>
2021-12-28 18:26:26 -0800

    Working on riddler.py

--- _04_int/_1_riddler/riddler.py
+++ _04_int/_1_riddler/riddler.py
@@ -11,4 +11,8 @@
 * After all the riddles have been asked, tell the user how many they got
   correct
 """
-
+from tkinter import messagebox, simpledialog, Tk
+if __name__ == '__main__':
+    messagebox.showinfo(title='Riddle', message="You will be asked 3 riddles. At the end of the round, you will know how many you got right out of 3.")
+    Q1 = simpledialog.askstring(title='Question 1',prompt="David's father has 3 sons. Snap, Crackle and _____. What is the third son's name?")
+    if Q1 == 'David' or Q1 == 'david':
