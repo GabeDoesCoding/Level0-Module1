45a1050927cb81145747a7e9283f70e7cdd77595
GabeDoesCoding <gabelorensd@gmail.com>
2022-01-21 17:00:57 -0800

    Working on riddler.py

--- _04_int/_1_riddler/riddler.py
+++ _04_int/_1_riddler/riddler.py
@@ -14,10 +14,17 @@
 A = 0
 from tkinter import messagebox, simpledialog, Tk
 if __name__ == '__main__':
+    window = Tk()
+    window.withdraw()
     messagebox.showinfo(title='Riddle', message="You will be asked 3 riddles. At the end of the round, you will know how many you got right out of 3.")
     Q1 = simpledialog.askstring(title='Question 1',prompt="David's father has 3 sons. Snap, Crackle and _____. What is the third son's name?")
     if Q1 == 'David' or Q1 == 'david':
-        A + 1
+       A = A + 1
     Q2 = simpledialog.askstring(title='Question 2', prompt= "You walk into a cabin and it is pitch black. There is a lantern, a candle and a fireplace. You only have one match. Which do you light first? Answer starts with The, and words are capitalized")
     if Q2 == "The Match":
-        A + 1
+       A = A + 1
+    Q3 = simpledialog.askstring(title='Question 3', prompt="What has roots as nobody sees, is taller than trees, up, up it goes, and yet never grows?")
+    if Q3 == "A mountain":
+       A = A + 1
+
+    messagebox.showinfo(title='You finished!', message="You got " + str(A) + " correct!")
--- _04_int/_2_simple_number_adder/simple_adder.py
+++ _04_int/_2_simple_number_adder/simple_adder.py
@@ -3,3 +3,9 @@
 
 * Display the sum of the two numbers to the user
 """
+from tkinter import messagebox, simpledialog, Tk
+if __name__ == '__main__':
+    window = Tk()
+    window.withdraw()
+    Num1 = simpledialog.askinteger(title='Simple Adder', prompt="Enter in the first number you want to add")
+    Num2 = simpledialog.askinteger(title='Simple Adder', prompt="Enter in the second number you want to add")
