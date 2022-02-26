9bbb63d0702804dc19b3b8a1b4d12213fb6e15fd
GabeDoesCoding <gabelorensd@gmail.com>
2022-02-25 17:01:50 -0800

    Working on simple calculator

--- _04_int/_3_simple_calculator/simple_calculator.py
+++ _04_int/_3_simple_calculator/simple_calculator.py
@@ -6,3 +6,9 @@
 * Use if-else statements to provide the desired math operation on the numbers
   and display the result.
 """
+from tkinter import messagebox, simpledialog, Tk
+if __name__ == '__main__':
+    window = Tk()
+    window.withdraw()
+    Calculate = simpledialog.askinteger(title="Simple calculator",prompt="Which calculator function would you like to use? Type 1 for addition, 2 for subtraction, 3 for multiplication and 4 for division.")
+    if Calculate == "1":
