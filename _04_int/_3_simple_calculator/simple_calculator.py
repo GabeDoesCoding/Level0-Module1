e198c0f770984859a0887590944be61450a79b63
GabeDoesCoding <gabelorensd@gmail.com>
2022-03-11 16:59:13 -0800

    Working on simple calculator

--- _04_int/_3_simple_calculator/simple_calculator.py
+++ _04_int/_3_simple_calculator/simple_calculator.py
@@ -12,7 +12,22 @@ if __name__ == '__main__':
     window.withdraw()
     Calculate = simpledialog.askinteger(title="Simple calculator",prompt="Which calculator function would you like to use? Type 1 for addition, 2 for subtraction, 3 for multiplication and 4 for division.")
     if Calculate == 1:
-        AddNum1 = simpledialog.askinteger(title='Simple Adder', prompt="Enter in the first number you want to add")
-        AddNum2 = simpledialog.askinteger(title='Simple Adder', prompt="Enter in the second number you want to add")
-        Ans = AddNum1 + AddNum2
-        messagebox.showinfo(title='Simple Adder', message="The answer is "+ str(Ans) +"!")
+        AddNum1 = simpledialog.askfloat(title='Simple Addition', prompt="Enter in the first number you want to add.")
+        AddNum2 = simpledialog.askfloat(title='Simple Addition', prompt="Enter in the second number you want to add.")
+        AddAns = AddNum1 + AddNum2
+        messagebox.showinfo(title='Simple Addition', message="The answer is "+ str(AddAns) +"!")
+    elif Calculate == 2:
+        SubNum1 = simpledialog.askfloat(title='Simple Subtraction', prompt="Enter in the first number you want to subtract.")
+        SubNum2 = simpledialog.askfloat(title='Simple Subtraction', prompt="Enter in the second number you want to subtract.")
+        SubAns = SubNum1 - SubNum2
+        messagebox.showinfo(title='Simple Subtraction', message="The answer is " + str(SubAns) +"!")
+    elif Calculate == 3:
+        MultNum1 = simpledialog.askfloat(title='Simple Multiplication', prompt="What is the first number you want to multiply?")
+        MultNum2 = simpledialog.askfloat(title='Simple Multiplication', prompt="What is the second number you want to multiply?")
+        MultAns = MultNum1 * MultNum2
+        messagebox.showinfo(title='Simple Multiplication', message=" The answer is " +str(MultAns) +"!")
+    elif Calculate == 4:
+        DivNum1 = simpledialog.askfloat(title='Simple Division', prompt="What is the first number you want to divide?")
+        DivNum2 = simpledialog.askfloat(title='Simple Division', prompt="What is the second number you want to divide?")
+        DivAns = DivNum1 / DivNum2
+        messagebox.showinfo(title='Simple Division', message="The answer is " +str(DivAns) +"!")
