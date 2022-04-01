2166aafb71edfff5710404ae4b9480c9e560a74b
GabeDoesCoding <gabelorensd@gmail.com>
2022-04-01 16:58:27 -0700

    Finished up to banana split.

---
+++ _05_for_loops/_2_badgers/Badgers.py
@@ -0,0 +1,9 @@
+
+
+if __name__ == '__main__':
+    for i in range(2):
+        for x in range(12):
+            print("Badger, ")
+        for z in range(2):
+            print("Mushroom, ")
+    print("A snake!!!")
